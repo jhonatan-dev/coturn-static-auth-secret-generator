b8c4079ca9380ec686690749272b97fb8ad11878
Jhonatan Castillo Salvador <45980089+jhonatan-dev@users.noreply.github.com>
2020-10-12 20:32:54 -0500

    Update credentials.js

--- utils/credentials.js
+++ utils/credentials.js
@@ -9,7 +9,7 @@ module.exports = {
     //Las credenciales serán válidas por las próximas 24 horas
     let unixTimeStamp = parseInt(Date.now() / 1000) + time_limit * 3600;
     let username = [unixTimeStamp, nombreUsuario].join("_");
-    hmac = crypto.createHmac("sha1", hashAuthSecret);
+    let hmac = crypto.createHmac("sha1", hashAuthSecret);
     hmac.setEncoding("base64");
     hmac.write(username);
     hmac.end();
